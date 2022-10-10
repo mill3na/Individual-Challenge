a26ae18207d4aab510e2d593907f1b5c2065c24e
Milena Maia <milenamaiaaraujo@gmail.com>
2022-10-10 10:43:52 -0300

    :tada: init: init project

---
+++ Individual-Challenge/Individual-Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Individual-Challenge
+//
+//  Created by Milena Maia Araújo on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
