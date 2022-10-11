1e2875367837ceed11f51345b111aae3ae3bff51
Milena Maia <milenamaiaaraujo@gmail.com>
2022-10-11 10:48:27 -0300

    :sparkle: updated api connection

--- Individual-Challenge/Individual-Challenge/ViewController.swift
+++ Individual-Challenge/Individual-Challenge/ViewController.swift
@@ -11,7 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let apicall = API()
+//        apicall.getAllRecipes()
+//        apicall.getRecipeByQuery(query: "rice")
+//        apicall.getRecipeInstructions(id: 404784)
     }
 
 }
