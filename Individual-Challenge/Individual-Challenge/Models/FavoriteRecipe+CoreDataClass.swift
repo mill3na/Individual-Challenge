3dfd0bbe5839607f54d3d939a8d7078410f951f2
Milena Maia <milenamaiaaraujo@gmail.com>
2022-10-14 09:36:38 -0300

    :sparkles: feat: initiate core data methods implementation

---
+++ Individual-Challenge/Individual-Challenge/Models/FavoriteRecipe+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  FavoriteRecipe+CoreDataClass.swift
+//  Individual-Challenge
+//
+//  Created by Milena Maia Araújo on 14/10/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(FavoriteRecipe)
+public class FavoriteRecipe: NSManagedObject {
+
+}
